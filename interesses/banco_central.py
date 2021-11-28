65e7faa0cb4b999ad213c4ddfb0fefcb5a3a5646
Matheus Santiago <matheus.n.santiago99@gmail.com>
2021-11-28 18:32:27 -0300

    criar classes que vão agrupar certas buscas

---
+++ interesses/banco_central.py
@@ -0,0 +1,70 @@
+from utils import ColumnSearch
+import pandas as pd
+
+
+class BancoCentral:
+    def __init__(self, search):
+        self.search = search
+        self._df = self.search.df
+
+    def publicacoes_DO1(self):
+        """Todos as publicacões que não sejam Instruções normativas do Banco Central no DO1 entram na súmula"""
+
+        return self.search.keyword_search(
+            searches=[
+                ColumnSearch([self._df.escopo], ["Banco Central"]),
+                ColumnSearch([self._df.secao], ["DO1"]),
+            ],
+            where=(self._df.tipo_normativo != "Instrução Normativa"),
+        ).assign(motivo="Publicação do BC no DO1 que não é intrução normativa")
+
+    def assinaturas_dos_diretores_e_presidente(self):
+        """Qualquer coisa assinada por um diretor/presidente do BC entra na súmula
+
+        Ao comparar as assinaturas, ele vai remover as acentuações, uma vez que o uso de acentuação é bem inconsistente
+        """
+        return self.search.keyword_search(
+            searches=[
+                ColumnSearch(
+                    columns=[self._df.assinatura],
+                    keywords=ASSINATURAS_DIRETORES_E_PRESIDENTE_DO_BC,
+                )
+            ]
+        ).assign(motivo="Assinatura de um diretor ou pelo presidente do BC")
+
+    def orgao_importante_menciona_o_banco_central_na_ementa(self):
+        por_escopo = self.search.keyword_search(
+            searches=[
+                ColumnSearch(
+                    [self._df.escopo], ["Secretaria Especial do Tesouro e Orçamento"]
+                ),
+                ColumnSearch([self._df.ementa], ["Banco Central"]),
+            ]
+        ).assign(
+            motivo="Publicação da Secretaria Especial do Tesouro e Orçamento que contém 'Banco Central' na ementa"
+        )
+
+        por_titulo = self.search.keyword_search(
+            searches=[
+                ColumnSearch([self._df.titulo], ["SUSEP", "PREVIC", "CONAF"]),
+                ColumnSearch([self._df.ementa], ["Banco Central"]),
+            ]
+        ).assign(
+            motivo="Publicação da SUSEP, PREVIC ou CONAF que contém 'Banco Central' na ementa"
+        )
+
+        return pd.concat([por_escopo, por_titulo]).drop_duplicates(subset="id")
+
+
+ASSINATURAS_DIRETORES_E_PRESIDENTE_DO_BC = [
+    # * = Não aparece nenhuma vez no meu banco de dados
+    "ROBERTO DE OLIVEIRA CAMPOS NETO",  # Presidente
+    "Maurício Costa de Moura",  # * Diretor de Relacionamento, Cidadania e Supervisão de Conduta - Direc
+    "Paulo sérgio Neves Souza",  # * Diretor de Fiscalização - Difis
+    "Fabio Kanczuk",  # * Diretor de Política Econômica - Dipec
+    "Bruno Serra Fernandes",  # Diretor de Política Monetária - Dipom
+    "Fernanda Guardado",  # * Diretora de Assuntos Internacionais e de Gestão de Riscos Corporativos - Direx
+    "João Manoel Pinho de Mello",  # Diretor de Organização do Sistema Financeiro e de Resolução - Diorf
+    "Otávio Ribeiro Damaso",  # * Diretor de Regulação - Dinor
+    "Carolina de Assis Barros",  # Diretora de Administração - Dirad
+]
---
+++ interesses/geral.py
@@ -0,0 +1,87 @@
+import pandas as pd
+from utils import ColumnSearch
+
+
+class Geral:
+    def __init__(self, search):
+        self.search = search
+        self._df = self.search.df
+
+    def atos_e_resolucoes_do_CMN(self):
+        """Qualquer ato do CMN entra na súmula"""
+
+        return self.search.keyword_search(
+            searches=[ColumnSearch([self._df.titulo], [r"\sCMN\s"])]
+        ).assign(motivo="Ato do CMN")
+
+    def posse_e_exoneracao_de_cargo(self):
+        """Toda posse/exoneração de um cargo importante vai pra súmula"""
+
+        return self.search.keyword_search(
+            searches=[ColumnSearch([self._df.conteudo], TERMOS_DE_POSSE)],
+        ).assign(motivo="posse e exoneração de cargo")
+
+    def coaf(self):
+        """
+        - Quando o presidente do COAF se ausenta, é substituido em caso de férias ou faz uma viagem
+        - Quando uma Resolução é assinada pelo presidente do COAF no DO1
+        """
+
+        # Sempre que o presidente do COAF se ausenta, o Presidente do Banco Central do Brasil precisa fazer um despacho
+        ausencia_do_presidente = self.search.keyword_search(
+            searches=[
+                ColumnSearch(
+                    [self._df.conteudo],
+                    [
+                        "Despacho do Presidente do Banco Central do Brasil.+Presidente do COAF"
+                    ],
+                ),
+            ]
+        ).assign(motivo="Presidente do COAF se ausentou (férias, substituído, etc)")
+
+        resoluções_assinadas_pelo_presidente = self.search.keyword_search(
+            searches=[ColumnSearch([self._df.assinatura], keywords=["RICARDO LIÁO"])],
+            where=(self._df.secao.str.contains("DO1"))
+            & (self._df.tipo_normativo == "Resolução"),
+        ).assign(motivo="Resolução assinada pelo presidente do COAF")
+
+        return pd.concat(
+            [ausencia_do_presidente, resoluções_assinadas_pelo_presidente]
+        ).drop_duplicates(subset="id")
+
+TERMOS_DE_POSSE = [
+    # Assunto 6:
+    "cargo de Presidente do Banco Central do Brasil",
+    # Assunto 7:
+    "cargo de Diretor do Banco Central do Brasil",
+    "cargo de Diretora do Banco Central do Brasil",
+    # Assunto 11:
+    "cargo de Secretário Especial de Fazenda do Ministério da Economia",
+    # Assunto 12:
+    "cargo de Secretário-Executivo do Ministério da Economia",
+    # Assunto 13:
+    "cargo de Secretário de Política Econômica",
+    # Assunto 14:
+    "cargo de Secretário do Tesouro Nacional",
+    # Assunto 15:
+    "cargo de Presidente da Casa da Moeda do Brasil",
+    # Assunto 16:
+    "cargo de Diretor da Comissão de Valores Mobiliários",
+    # Assunto 17:
+    "cargo de Superintendente da Superintendência de Seguros Privados",
+    # Assunto 18:
+    "cargo de Diretor da Superintendência de Seguros Privados",
+    # Assunto 19:
+    "cargo de Diretor-Superintendente da Superintendência Nacional de Previdência Complementar",
+    # Assunto 20:
+    "cargo de Diretor de Licenciamento da Superintendência Nacional de Previdência Complementar",
+    # Assunto 21:
+    "cargo de Secretário Especial Adjunto da Secretaria Especial de Previdência e Trabalho do Ministério da Economia",
+    # Assunto 22:
+    "cargo de Secretário-Executivo do Ministério do Trabalho e Previdência",
+    ###################################################################
+    # Assunto 10:
+    "cargo de Ministro de Estado da Economia",
+    #
+    "cargo de Ministro de Estado do Trabalho e Previdência",
+]

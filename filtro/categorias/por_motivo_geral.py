72dd0b2b133dae051a6ea49d3bb8a6b78fea7d11
Matheus Santiago <matheus.n.santiago99@gmail.com>
2021-12-22 15:10:03 -0300

    .

--- filtro/categorias/por_motivo_geral.py
+++ filtro/categorias/por_motivo_geral.py
@@ -1,12 +1,14 @@
 from ..filtro import Filtro
 from teste import Pattern
 
+
 class FiltragemPorMotivoGeral(Filtro):
     def publicacoes_BC_DO1_nao_IN(self):
         return self.query(
-            self.contains(self._df.escopo, "Banco Central")
-            & self.contains(self._df.secao, "DO1")
-            & (self._df.tipo_normativo != "Instrução Normativa"),
+            (self.contains(self._df.escopo, "Banco Central"))
+            & (self.contains(self._df.secao, "DO1"))
+            & (self._df.tipo_normativo != "Instrução Normativa")
+            & (self._df.tipo_normativo != "Retificação"),
             motivo="Publicação do Banco Central no DO1 que não é instrução normativa",
         )
 

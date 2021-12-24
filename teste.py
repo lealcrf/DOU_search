c8992ef516f3ce9b541a652cd9d930aaf667e6c2
Matheus Santiago <matheus.n.santiago99@gmail.com>
2021-12-24 07:51:09 -0300

    transformar Pattern em classe

--- teste.py
+++ teste.py
@@ -1,10 +1,20 @@
-import pandas as pd
-from collections import namedtuple
-import re
+from dataclasses import dataclass
+from typing import List
 
 
+@dataclass
+class Pattern:
+    regex: str
+    motivo: str = None
 
+    def completar_motivo(self, coluna):
+        motivo = "• " + self.motivo if self.motivo else self.criar_motivo_generico(coluna)
+        padrao_estipulado = "{}[{}]".format(coluna, self.regex)
+        
+        return motivo + " => " + padrao_estipulado
 
+    def criar_motivo_generico(self, coluna):
+        if coluna in ["assinatura", "ementa"]:
+            return "Achou \"{}\" na {}".format(self.regex, coluna)
 
-
-Pattern = namedtuple("Pattern", ["regex", "motivo"])
+        return "Achou \"{}\" no {}".format(self.regex, coluna)

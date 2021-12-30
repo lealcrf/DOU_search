6a78b5ee0cae12fd110cf3b5db7c8a4478535e5b
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2021-12-30 00:37:00 -0300

    adicionar função que limpa a publicação pra sumula

--- model.py
+++ model.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
 from datetime import date
+import re
+
 
 @dataclass
 class Publicacao:
@@ -9,7 +11,7 @@ class Publicacao:
 
     """
 
-    id: str 
+    id: str
     secao: str
     tipo_normativo: str
     data: date
@@ -18,5 +20,44 @@ class Publicacao:
     ementa: str
     conteudo: str
     assinatura: str
-    pdf:str
-    id_materia:str
+    pdf: str
+    id_materia: str
+    motivo: str = None
+
+    def to_sumula(self) -> dict:
+        """Limpa a publicação para ser enviada à súmula"""
+
+        # | Tira o excesso do titulo
+        self.titulo = re.sub(r",? DE .+2021?", "", self.titulo.upper())
+
+        escopos_importantes = [
+            "Banco Central do Brasil",
+            "Ministério da Economia",
+            "Presidência da República",
+            "Conselho de Controle de Atividades Financeiras",
+            "Atos do Poder Legislativo",
+            "Atos do Poder Executivo",
+            "Ministério Público da União",
+            "Ministério do Trabalho e Previdência",
+        ]
+
+        # | Caso alguma parte escopo for particularmente importante, deixa só a parte importante como escopo
+        for escopo in self.escopo.split("/")[::-1]:  # por grau de importância
+            if escopo in escopos_importantes:
+                self.escopo = escopo  # Muda o escopo para ser só o importante
+                break
+            else:
+                self.escopo = "Outros"
+
+        self.ementa = self.ementa if self.ementa else ""
+
+        # | Retorna só as partes que importam para a súmula
+        return {
+            "titulo": self.titulo,
+            "url": self.pdf,
+            "ementa": self.ementa,
+            "escopo": self.escopo,
+            # API do teams precisa que o " seja escapado. Já que, por exemplo, no payload ele vai fazer tipo assim:
+            # "text": "A publicação contem "lorem ipsilum" em x". aspas se cancelam
+            "motivo": self.motivo.replace('"', r"\""),  
+        }

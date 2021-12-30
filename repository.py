02af1465a9bab0b892811c225dee83e296a0b58a
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2021-12-30 00:38:51 -0300

    função que envia minha súmula para o teams

--- repository.py
+++ repository.py
@@ -9,10 +9,11 @@ from firebase_admin import firestore
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+import requests
+import json
 
-ja_inicializou = False
 
-if not ja_inicializou:
+try:
     # Firestore
     firebase_admin.initialize_app(
         credential=credentials.Certificate("cred.json"),
@@ -28,8 +29,10 @@ if not ja_inicializou:
         database="test",
     )
     cursor = conn.cursor()
-    
+
     ja_inicializou = True
+except:
+    pass
 
 
 def pegar_publicacoes_do_DOU_DB(n_ultimas_publicacoes: int = None) -> DataFrame:
@@ -50,7 +53,7 @@ def pegar_publicacoes_do_DOU_DB(n_ultimas_publicacoes: int = None) -> DataFrame:
     df = pd.DataFrame(Publicacao(*pub) for pub in cursor.fetchall())
 
     return df
-    
+
 
 def inserir_publicacoes_sumulaDB(df: DataFrame):
     """Coloca as publicações na base de dados da súmula (firestore)"""
@@ -65,17 +68,50 @@ def inserir_publicacoes_sumulaDB(df: DataFrame):
 def get_link_da_publicacao_ingov(id_materia: str) -> str:
     """Faz um scrape para achar o link da do site in.gov baseado no id da matéria"""
 
-    print(id_materia)
-
     options = webdriver.ChromeOptions()
     options.headless = True
     driver = webdriver.Chrome(options=options)
-    driver.get(
-        f'https://www.in.gov.br/consulta/-/buscar/dou?q="{id_materia}"&s=todos&exactDate=all&sortType=0'
-    )
+
+    try:
+        driver.get(
+            f'https://www.in.gov.br/consulta/-/buscar/dou?q="{id_materia}"&s=todos&exactDate=all&sortType=0'
+        )
+        print("✅ " + id_materia)
+    except:
+        print("❌ " + id_materia)
 
     element: WebElement = driver.find_element(By.XPATH, "//h5[@class='title-marker']/a")
     link = element.get_property("href")
 
     driver.close()
     return link
+
+
+def enviar_sumula_para_o_teams(sumula: DataFrame):
+    # | Agrupar publicações por escopo (secoes)
+    secoes = dict()
+
+    for pub in sumula.transpose().to_dict().values():
+        pub = Publicacao(**pub).to_sumula()
+        secoes.setdefault(pub["escopo"], []).append(pub)
+
+    # Ordenar as publicações de cada espopo em ordem alfabética do título
+    for escopo, pubs in secoes.items():
+        secoes[escopo] = sorted(pubs, key=lambda pub: pub["titulo"])
+
+    # | Enviar a publicação para o Teams
+    dia = sumula.iloc[0].data.strftime("%d/%m/%Y")
+    payload = {
+        "cabecalho": "Publicações do dia " + dia,
+        "escopos": [
+            {"escopo": escopo, "publicacoes": pubs} for escopo, pubs in secoes.items()
+        ],
+    }
+
+    flow_api = "https://prod-19.brazilsouth.logic.azure.com:443/workflows/c067f4e36ce342489f14345294f117e1/triggers/manual/paths/invoke?api-version=2016-06-01&sp=%2Ftriggers%2Fmanual%2Frun&sv=1.0&sig=pECFeZEKwawc4dsOMD9XBtpNKc3f3zBsTgRZbQ9Vq1k"
+
+    requests.post(
+        flow_api,
+        data=json.dumps(payload),
+        headers={"content-type": "application/json"},
+    )

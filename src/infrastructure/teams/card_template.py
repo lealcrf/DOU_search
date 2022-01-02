8346487174153105a8600f125b61c5784806c4e1
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-02 00:30:10 -0300

    integraçao com teams

---
+++ src/infrastructure/teams/card_template.py
@@ -0,0 +1,83 @@
+import json
+import math
+
+
+def publicacao_to_card(pub):
+
+    rows_necessarias = math.ceil(len(pub["motivos"]) / 2)
+
+    body = [
+        titulo(pub["titulo"], pub["url"]),
+        ementa(pub["ementa"]),
+        *[row(pub["motivos"], n_row) for n_row in range(rows_necessarias)],
+    ]
+
+    return "{}".format(
+        json.dumps(
+            {
+                "type": "AdaptiveCard",
+                "body": body,
+                "msteams": {"width": "Full"},
+                "$schema": "http://adaptivecards.io/schemas/adaptive-card.json",
+                "version": "1.2",
+            }
+        ),
+    )
+
+
+def titulo(titulo, url):
+    return {
+        "type": "Container",
+        "items": [
+            {
+                "type": "TextBlock",
+                "size": "Large",
+                "weight": "Bolder",
+                "text": f"[{titulo}]({url})",
+                "color": "Accent",
+                "horizontalAlignment": "Center",
+            }
+        ],
+        "style": "emphasis",
+    }
+
+
+def ementa(ementa):
+    return {
+        "type": "Container",
+        "items": [
+            {
+                "type": "TextBlock",
+                "text": ementa,
+                "wrap": True,
+            },
+        ],
+    }
+
+
+def row(motivos, n_row):
+    index_motivo_inicial = n_row * 2
+    index_motivo_final = (2 * (n_row + 1)) - (len(motivos) % 2)
+
+    return {
+        "type": "ColumnSet",
+        "columns": [
+            {
+                "type": "Column",
+                "width": "stretch",
+                "items": [
+                    # | Divide a lista em pedaços de 2, preenchendo os slots disponíveis com os motivos correspondentes
+                    {
+                        "type": "TextBlock",
+                        "text": motivos[index],
+                        "wrap": True,
+                    }
+                    for index in range(index_motivo_inicial, index_motivo_final)
+                ],
+                "style": "emphasis",
+                "spacing": "Small",
+            }
+        ],
+    }
+
+
---
+++ src/infrastructure/teams/teams.py
@@ -0,0 +1,52 @@
+from pandas import DataFrame
+from ..models.publicacao import Publicacao
+import json
+import requests
+from .card_template import publicacao_to_card
+from config import FLOW_ENDPOINT
+
+
+def enviar_sumula_para_o_teams(sumula: DataFrame):
+    secoes = limpar_e_ordenar_sumula_em_secoes(sumula)
+
+    # TODO Faz alguma coisa para quando a súmula vier vazia, já que isso vai dar erro
+    dia = sumula.iloc[0].data.strftime("%d/%m/%Y")
+
+    payload = {
+        "cabecalho": "Publicações do dia " + dia,
+        "escopos": [
+            {
+                "escopo": escopo,
+                "publicacoes": [{"publicacao": "<pub>"} for _ in pubs],
+            }
+            for escopo, pubs in secoes.items()
+        ],
+    }
+
+    for index_escopo, escopo in enumerate(secoes.keys()):
+        publicacoes = payload["escopos"][index_escopo]["publicacoes"]
+
+        for index_publicacao, item in enumerate(publicacoes):
+            publicacao = secoes[escopo][index_publicacao]
+
+            item["publicacao"] = publicacao_to_card(publicacao)
+
+    requests.post(
+        FLOW_ENDPOINT,
+        data=json.dumps(payload),
+        headers={"content-type": "application/json"},
+    )
+
+
+def limpar_e_ordenar_sumula_em_secoes(sumula: DataFrame) -> dict[str, list]:
+    secoes = dict()
+
+    for pub in sumula.transpose().to_dict().values():
+        pub = Publicacao(**pub).to_sumula()
+        secoes.setdefault(pub["escopo"], []).append(pub)  # Limpa cada publicação
+
+    # Ordenar as publicações de cada espopo em ordem alfabética do título
+    for escopo, pubs in secoes.items():
+        secoes[escopo] = sorted(pubs, key=lambda pub: pub["titulo"])
+
+    return secoes

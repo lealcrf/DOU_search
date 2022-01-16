beb45eebc8ddbf84e4685fd320e8fb97ab6e5002
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-16 03:41:21 -0300

    remove filtragem por motivo geral

--- src/dou.py
+++ src/dou.py
@@ -3,12 +3,10 @@ import pandas as pd
 from pandas.core.frame import DataFrame
 
 from .utils import tirar_acentuacao
-from .filtro.filtro import Filtro
 from .filtro.categorias.por_assinatura import FiltragemPorAssinatura
 from .filtro.categorias.por_conteudo import FiltragemPorConteudo
 from .filtro.categorias.por_ementa import FiltragemPorEmenta
 from .filtro.categorias.por_escopo import FiltragemPorEscopo
-from .filtro.categorias.por_motivo_geral import FiltragemPorMotivoGeral
 from .filtro.categorias.por_titulo import FiltragemPorTitulo
 from .infrastructure.repository import pegar_url_do_ingov
 
@@ -35,17 +33,12 @@ class DOU():
         return FiltragemPorEscopo(self.df)
 
     @property
-    def filtrar_por_motivo_geral(self):
-        return FiltragemPorMotivoGeral(self.df)
-
-    @property
     def filtrar_por_titulo(self):
         return FiltragemPorTitulo(self.df)
 
     def gerar_sumula(self, com_link_ingov=False) -> pd.DataFrame:
         resultado = pd.concat(
             [
-                # # self.filtrar_por_motivo_geral(),
                 self.filtrar_por_titulo(),
                 self.filtrar_por_escopo(),
                 self.filtrar_por_ementa(),

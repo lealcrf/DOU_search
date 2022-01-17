a5b62fe73aa17013625cc35969702a5a563b6b42
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-17 14:42:01 -0300

    add DateRange utils class

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,6 @@
 import unicodedata
+from datetime import date, timedelta
+
 
 def tirar_acentuacao(string: str):
     if string is not None:
@@ -6,4 +8,22 @@ def tirar_acentuacao(string: str):
             c
             for c in unicodedata.normalize("NFD", string)
             if unicodedata.category(c) != "Mn"
+        )
+
+
+class DateRange:
+    def __init__(self, inicio: date, fim: date):
+        self.inicio = inicio
+        self.fim = fim
+
+    @staticmethod
+    def same_day(day, month=date.today().month, year=date.today().year):
+        data = date(year, month, day)
+        return DateRange(data, data)
+
+    @staticmethod
+    def from_ultimos_n_dias(n_dias: int, ultimo_dia=date.today()):
+        return DateRange(
+            inicio=ultimo_dia - timedelta(days=n_dias),
+            fim=ultimo_dia,
         )

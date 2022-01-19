880dbcc29d2cd1f004ca0c713dceef1d6217a2ad
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-19 17:09:43 -0300

    mudar organização/limpeza da súmula pro script aws

--- aws/gerar_sumula/lambda_function.py
+++ aws/gerar_sumula/lambda_function.py
@@ -1,13 +1,14 @@
 from dotenv import load_dotenv
 from src.dou import DOU
-from src.utils import DateRange, str_to_date
-from src.infrastructure.repository import query_dou_remote
+from src.models.publicacao import Publicacao
+from src.utils import DateRange
+import src.infrastructure.repository as repo
 
 load_dotenv()
 
 
 def lambda_handler(event, context):
-    ultimos_dois_dous = query_dou_remote(
+    ultimos_dois_dous = repo.query_dou_remote(
         "SELECT DISTINCT TOP 2 * FROM c.data as d ORDER BY d DESC"
     )
 
@@ -16,15 +17,34 @@ def lambda_handler(event, context):
 
     dou = DOU(
         date_range=DateRange(data_inicial, data_final),
-        is_from_remote_db=True,
+        get_from_remote_db=True,
     )
-    
-    sumula = dou.gerar_sumula(versao_oficial=True)    
-    
-    if len(sumula) == 0:
+
+    sumula = dou.gerar_sumula()
+
+    if sumula.empty:
         return {
             "body": f"Não encontrou publicações nos DOUs do dia {data_inicial} (pub extra) e {data_final}",
             "status": "VAZIO",
         }
 
-    return {"body": sumula, "status": "OK"}
+    # | Organiza as publicações de modo a se assemelhar à súmula oficial
+    # Tira as publicações que não são extras da súmula passada
+    unique_dates = sumula["data"].sort_values().unique()
+    if len(unique_dates) == 2:
+        is_from_last_edition = sumula.data == sumula.data.min()
+        is_not_extra = sumula.secao.str.contains("DO[123]$")
+
+        sumula = sumula[~(is_from_last_edition & is_not_extra)]
+
+    # Divide as publicações por escopo (i.e. seções)
+    sumula_seccionada = dict()
+    for pub in sumula.transpose().to_dict().values():
+        pub = Publicacao(**pub).to_sumula()
+        sumula_seccionada.setdefault(pub["escopo"], []).append(pub)
+
+    # Coloca as publicações de cada escopo em ordem alfabética
+    for escopo, pubs in sumula_seccionada.items():
+        sumula_seccionada[escopo] = sorted(pubs, key=lambda pub: pub["titulo"])
+
+    return {"body": sumula_seccionada, "status": "OK"}

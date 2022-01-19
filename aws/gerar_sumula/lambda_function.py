0e7cfbee688722ad4bd82b141d55a3600cda2ef5
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-19 09:02:13 -0300

    add error handling + pegar os 2 últimos DOUs

--- aws/gerar_sumula/lambda_function.py
+++ aws/gerar_sumula/lambda_function.py
@@ -1,27 +1,30 @@
-from datetime import date
 from dotenv import load_dotenv
 from src.dou import DOU
-from src.utils import DateRange
+from src.utils import DateRange, str_to_date
+from src.infrastructure.repository import query_dou_remote
 
 load_dotenv()
 
 
 def lambda_handler(event, context):
+    ultimos_dois_dous = query_dou_remote(
+        "SELECT DISTINCT TOP 2 * FROM c.data as d ORDER BY d DESC"
+    )
 
-    data_inicial = date(2022, 1, 7)
-    data_final = date(2022, 1, 7)
+    data_inicial = ultimos_dois_dous[1]
+    data_final = ultimos_dois_dous[0]
 
     dou = DOU(
         date_range=DateRange(data_inicial, data_final),
-        is_remote_db=True,
+        is_from_remote_db=True,
     )
-
-    if dou.df.empty:
+    
+    sumula = dou.gerar_sumula(versao_oficial=True)    
+    
+    if len(sumula) == 0:
         return {
-            "body": f"Não encontrou publicações nos DOUs do dia {data_inicial} (apenas publicações extras) e {data_final}",
+            "body": f"Não encontrou publicações nos DOUs do dia {data_inicial} (pub extra) e {data_final}",
             "status": "VAZIO",
         }
-
-    sumula = dou.gerar_sumula(versao_oficial=True)
 
     return {"body": sumula, "status": "OK"}

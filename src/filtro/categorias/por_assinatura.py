a47ed4404bc9ef663591649eae5cc7aed9335674
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-29 00:14:29 -0300

    atualizar por_assinatura engine nova

--- src/filtro/categorias/por_assinatura.py
+++ src/filtro/categorias/por_assinatura.py
@@ -2,53 +2,51 @@ from ..filtro import Filtro, Pattern
 
 class FiltragemPorAssinatura(Filtro):
     def assinadas_pelo_presidente_do_COAF(self):
-        return self.query(
-            (self.contains(self.df.assinatura, "RICARDO LIÁO"))  # A28
-            & (self.df.tipo_normativo == "Portaria")  # A28
-            # & (self.contains(self._df.secao, "DO1"))  # R2A1
-        ).assign(motivo="Portaria assinada pelo pelo presidente do COAF")
+        yield from [
+            Pattern(  # A28
+                self.assinatura.contem("RICARDO LIÁO")
+                & (self.tipo_normativo == "Portaria")
+                & self.secao.contem("DO1"),  # R2A1
+                motivo="Portaria assinada pelo pelo presidente do COAF",
+            )
+        ]
 
     def presidente_e_diretores_do_BC(self):
-        return self.query(
-            self.contains(
-                self.df.assinatura,
-                [
-                    Pattern(
-                        "ROBERTO DE OLIVEIRA CAMPOS NETO",
-                        "Assinatura do Presidente do BC",
-                    ),
-                    Pattern(
-                        "Maurício Costa de Moura",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Paulo sérgio Neves de Souza",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Fabio Kanczuk",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Bruno Serra Fernandes",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Fernanda Magalhaes Rumenos Guardado",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "João Manoel Pinho de Mello",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Otávio Ribeiro Damaso",
-                        "Assinatura de um diretor do BC",
-                    ),
-                    Pattern(
-                        "Carolina de Assis Barros",
-                        "Assinatura de um diretor do BC",
-                    ),
-                ],
+        yield from [
+            Pattern(
+                self.assinatura.contem(r"ROBERTO DE OLIVEIRA CAMPOS NETO"),
+                motivo="Assinatura do Presidente do BC",
             ),
-        )
+            Pattern(
+                self.assinatura.contem(r"Maurício Costa de Moura"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem(r"Paulo sérgio Neves de Souza"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem(r"Fabio Kanczuk"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem("Bruno Serra Fernandes"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem("Fernanda Magalhaes Rumenos Guardado"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem("João Manoel Pinho de Mello"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem("Otávio Ribeiro Damaso"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+            Pattern(
+                self.assinatura.contem("Carolina de Assis Barros"),
+                motivo="Assinatura de um diretor do BC",
+            ),
+        ]

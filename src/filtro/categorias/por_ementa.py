3454741add1f8e69bf2313611c2cb445309583bb
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-31 01:54:50 -0300

    Atualizar sintaxe dos critérios por ementa

--- src/filtro/categorias/por_ementa.py
+++ src/filtro/categorias/por_ementa.py
@@ -2,94 +2,117 @@ from ..filtro import Filtro, Criterio
 
 
 class FiltragemPorEmenta(Filtro):
-    def frases_gerais(self):
-        return self.query(
-            condicoes=self.contains(
-                self.df.ementa,
-                [
-                    Criterio("Conselho de Controle de Atividades Financeiras"),  # A[123]
-                    Criterio(
-                        "Comitê de Regulação e Fiscalização dos Mercados Financeiro, de Capitais, de Seguros, de Previdência e Capitalização"
-                    ),  # A[123]
-                    Criterio("Comitê de Estabilidade Financeira"),  # A[123]
-                    Criterio("Educação financeira"),  # A[123],
-                    Criterio("Comitê Nacional de Educação Financeira"),  # A[123]
-                    Criterio("Imposto sobre Operações Financeiras"),  # A[123]
-                    Criterio(
-                        "Programa Nacional de Apoio às Microempresas e Empresas de Pequeno Porte"
-                    ),  # A44
-                    Criterio("Lavagem de Dinheiro"),  # A45
-                    Criterio("Lei geral de proteção de dados"),  # R2A1
-                    Criterio("LGPD"),  # R2A1
-                    Criterio("Decreto nº 10.835"),  # R2A6
-                    Criterio(
-                        "Subdelega competências para a prática de atos de gestão de pessoas no âmbito do Ministério da Economia às autoridades que menciona"
-                    ),  # R2A7
-                    Criterio(
-                        "(?:Sistema de Pessoal Civil da Administração (?:Pública)? Federal|SIPEC)"
-                    ),  # R2A8
-                    Criterio("Lei nº 8.429"),  # R2A11
-                    Criterio("Lei nº 14.133"),  # R2A11
-                    Criterio("Programa de Estímulo ao Crédito"),  # R2Extra
-                    Criterio(
-                        "entidades da administração pública federal"
-                    ),  #! #TODO Temporário
-                    Criterio("Banco Central"),
-                ],
-            )
-        )
+    def gerais(self):
+        yield from [
+            Criterio(  # A[123]
+                self.ementa.contem(r"Conselho de Controle de Atividades Financeiras"),
+                motivo='"Conselho de Controle de Atividades Financeiras" na ementa',
+            ),
+            Criterio(  # A[123]
+                self.ementa.contem(
+                    "Comitê de Regulação e Fiscalização dos Mercados Financeiro, de Capitais, de Seguros, de Previdência e Capitalização",
+                ),
+                motivo='"Comitê de Regulação e Fiscalização dos Mercados Financeiro, de Capitais, de Seguros, de Previdência e Capitalização" na ementa',
+            ),
+            Criterio(  # A[123]
+                self.ementa.contem("Comitê de Estabilidade Financeira"),
+                motivo='"Comitê de Estabilidade Financeira" na ementa',
+            ),
+            Criterio(  # A[123],
+                self.ementa.contem("Educação financeira"),
+                motivo='"Educação financeira" na ementa',
+            ),
+            Criterio(  # A[123]
+                self.ementa.contem("Comitê Nacional de Educação Financeira"),
+                motivo='"Comitê Nacional de Educação Financeira" na ementa',
+            ),
+            Criterio(  # A[123]
+                self.ementa.contem("Imposto sobre Operações Financeiras"),
+                motivo='"Imposto sobre Operações Financeiras" na ementa',
+            ),
+            Criterio(  # A44
+                self.ementa.contem(
+                    "Programa Nacional de Apoio às Microempresas e Empresas de Pequeno Porte"
+                ),
+                motivo='"Programa Nacional de Apoio às Microempresas e Empresas de Pequeno Porte" na ementa',
+            ),
+            Criterio(  # A45
+                self.ementa.contem("Lavagem de Dinheiro"),
+                motivo='"Lavagem de Dinheiro" na ementa',
+            ),
+            Criterio(  # R2A1
+                self.ementa.contem("Lei geral de proteção de dados|LGPD"),
+                motivo='"Lei geral de proteção de dados" na ementa',
+            ),
+            Criterio(  # R2A6
+                self.ementa.contem("Decreto nº 10.835"),
+                motivo='"Decreto nº 10.835" na ementa',
+            ),
+            Criterio(  # R2A7
+                self.ementa.contem(
+                    "Subdelega competências para a prática de atos de gestão de pessoas no âmbito do Ministério da Economia às autoridades que menciona"
+                ),
+                motivo="#TODO ver o motivo do critério R2A7",
+            ),
+            Criterio(  # R2A8
+                self.ementa.contem(
+                    "(?:Sistema de Pessoal Civil da Administração (?:Pública)? Federal|SIPEC)"
+                ),
+                condicao="Publicação da SIPEC #TODO Terminar de fazer as restrições do R2A8",
+            ),
+            Criterio(  # R2A11
+                self.ementa.contem("Lei nº 8.429"),
+                motivo='"Lei nº 8.429" na ementa',
+            ),
+            Criterio(  # R2A11
+                self.ementa.contem("Lei nº 14.133"),
+                motivo='"Lei nº 14.133" na ementa',
+            ),
+            Criterio(  # R2Extra
+                self.ementa.contem("Programa de Estímulo ao Crédito"),
+                motivo='"Programa de Estímulo ao Crédito" na ementa',
+            ),
+            Criterio(
+                self.ementa.contem("entidades da administração pública federal"),
+                motivo='"entidades da administração pública federal" na ementa',
+            ),  #! #TODO Temporário
+        ]
 
-    def na_secao_1(self):
-        # TODO VER PORQUE O MEU FILTRO É TÃO LENTO, E VER SE TEM ALGUMA COISA QUE EU POSSO FAZER QUE SE PAREÇA COM ISSO
-        # return (
-        #     self._df.ementa.str.contains(
-        #         "(Poder Executivo Federal|administração pública federal direta e indireta)"
-        #     )
-        #     & self._df.secao.str.contains("DO1")
-        #     & self._df.escopo.str.contains(
-        #         "(Atos do Poder Executivo|Ministério da Economia)"
-        #     )
-        #     & (~self._df.titulo.str.contains("SETO\s?/\s?ME", na=False))
-        # )
+    def especificos(self):
+        yield from [
+            Criterio(  # R3A2
+                self.ementa.contem("poder executivo federal")
+                & self.secao.contem("DO1")
+                & self.escopo.contem("Atos do Poder Executivo")
+                & self.titulo.nao_contem(r"SETO\s?/\s?ME"),
+                motivo='Ato do Poder Executivo da seção 1 que contém "poder executivo federal" na ementa e não é SETO/ME',
+            ),
+            Criterio(  # R3A3
+                self.ementa.contem("poder executivo federal")
+                & self.secao.contem("DO1")
+                & self.escopo.contem("Ministério da Economia")
+                & self.titulo.nao_contem(r"SETO\s?/\s?ME"),
+                motivo='Publicação do Ministério da Economia da seção 1 que contém "poder executivo federal" na ementa e não é SETO/ME',
+            ),
+            Criterio(  # A4
+                self.ementa.contem(
+                    "Administração Pública federal direta, autárquica e fundacional"
+                )
+                & self.escopo.contem("Ministério da Economia")
+                & self.tipo_normativo.igual("Instrução Normativa"),
+                motivo='"Administração Pública federal direta, autárquica e fundacional" na ementa de Instruções Normativas do Ministério da Economia',
+            ),
+        ]
 
-        return self.query(
-            self.contains(
-                self.df.ementa,
-                [
-                    Criterio(
-                        "poder executivo federal",
-                        'Publicação com escopo "Atos do Poder Executivo" ou "Ministério da Economia" da seção 1 que contém "poder executivo federal" na ementa e não é SETO/ME  Secretaria do Tesouro',
-                    ),  # R3A2
-                    Criterio(
-                        "administração pública federal direta e indireta",
-                        'Publicação com escopo "Atos do Poder Executivo" ou "Ministério da Economia" da seção 1 que contém "administração pública federal direta e indireta" na ementa e não é SETO/ME  Secretaria do Tesouro',
-                    ),  # R3A3
-                ],
-            )
-            & self.contains(self.df.secao, "DO1")
-            & self.contains(
-                self.df.escopo,
-                [Criterio("Atos do Poder Executivo"), Criterio("Ministério da Economia")],
-            )
-            & (~self.contains(self.df.titulo, "SETO\s?/\s?ME")),
-            motivo='Publicações com escopo "Atos do Poder Executivo" e "Ministério da Economia" da seção 1 que contenham "poder executivo federal" ou "Administração Pública Federal Direta e Indireta" na ementa, excluindo as publicações da (SETO/ME) da Secretaria do Tesouro',
-        )  # https://teams.microsoft.com/l/message/19:1bfa966669b646c0a164a0731890fc03@thread.tacv2/1641231369273?tenantId=2a7030a7-3850-453e-96b7-cd5f58ba9ec2&groupId=b6ac4626-ee30-42ff-a204-c14fcbdb1313&parentMessageId=1641231369273&teamName=Projeto%20S%C3%BAmula%20DOU&channelName=Crit%C3%A9rios%20de%20busca&createdTime=1641231369273
-
-    def menciona_bc_no_conteudo(self):
-        return self.query(
-            self.contains(self.df.ementa, "Comitê Gestor da Segurança da Informação")
-            & self.contains(self.df.conteudo, "Banco Central"),
-            motivo="Comitê Gestor da Segurança da Informação na ementa e menciona o Banco Central no conteúdo",  # R2A10
-        )
-
-    def instrucao_normativa_misterio_da_economia_administracao_publica(self):
-        return self.query(
-            self.contains(
-                self.df.ementa,
-                "Administração Pública federal direta, autárquica e fundacional",
-            )
-            & self.contains(self.df.escopo, "Ministério da Economia")
-            & (self.df.tipo_normativo == "Instrução Normativa")  # A4,
-        ).assign(
-            motivo='• "Administração Pública federal direta, autárquica e fundacional" na ementa de Instruções Normativas do Ministério da Economia'
-        )
+    def menciona_o_banco_central(self):
+        yield from [
+            Criterio(
+                self.ementa.contem("Banco Central"),
+                motivo="Menciona o Banco Central na ementa",
+            ),
+            Criterio(  # R2A10
+                self.ementa.contem("Comitê Gestor da Segurança da Informação")
+                & self.conteudo.contem("Banco Central"),
+                motivo="Comitê Gestor da Segurança da Informação na ementa e menciona o Banco Central no conteúdo",
+            ),
+        ]

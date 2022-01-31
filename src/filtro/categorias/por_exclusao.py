da599a91b5823b22e5a17a355692f9f8524ac3ee
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-01-31 13:27:38 -0300

    .

--- src/filtro/categorias/por_exclusao.py
+++ src/filtro/categorias/por_exclusao.py
@@ -9,13 +9,3 @@ class FiltragemPorExclusao(Filtro):
                 & self.contains(self.df.escopo, "Banco Central")
             )
         )
-
-    # def teste(self):
-    #     yield from [
-    #         Criterio(  # R2A8
-    #             self.ementa.contem(
-    #                 r"(?:Sistema de Pessoal Civil da Administração (?:Pública)? Federal|SIPEC)"
-    #             ),
-    #             motivo="Publicação da SIPEC #TODO Terminar de fazer as restrições do R2A8",
-    #         ),
-    #     ]

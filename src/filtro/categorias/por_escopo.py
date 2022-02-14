76745e1e4a42d8f6c7806778a202d137abf06196
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-02-14 12:22:04 -0300

    Mudar critério R2A2 - T2022-2-13-Carlos

--- src/filtro/categorias/por_escopo.py
+++ src/filtro/categorias/por_escopo.py
@@ -9,9 +9,10 @@ class FiltragemPorEscopo(Filtro):
                 & self.conteudo.contem(r"Banco Central"),
                 motivo="Gabinete de Segurança Institucional no escopo e menciona Banco Central no conteúdo",
             ),
-            Criterio(
+            Criterio( #R2A2
                 self.escopo.contem(r"Secretaria Especial do Tesouro e Orçamento")
-                & self.ementa.contem(r"Banco Central"),
+                & self.conteudo.contem(r"Banco Central")  # T2022-2-13-Carlos
+                & self.titulo.notna(),
                 motivo="'Secretaria Especial do Tesouro e Orçamento' no escopo e menciona o Banco Central no conteúdo",
             ),
         ]

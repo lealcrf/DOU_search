b24cc91fcb4c31edd67ceedde596794409eb62fc
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-02-14 10:30:36 -0300

    .

--- src/infrastructure/repository.py
+++ src/infrastructure/repository.py
@@ -25,29 +25,3 @@ def pegar_dou_remote_db(date_range: DateRange):
     pubs = [Publicacao.from_database(json) for json in query_dou_remote(sql)]
 
     return pd.DataFrame(pubs, columns=Publicacao.get_fields())
-
-
-# def inserir_publicacoes_dou_db(df: DataFrame):
-#     """Coloca as publicações na database [dou] no (cosmosDB)"""
-
-#     client = CosmosClient(
-#         url=config.cosmos["ACCOUNT_URI"],
-#         credential=config.cosmos["ACCOUNT_KEY"],
-#     )
-#     db = client.get_database_client(config.cosmos["DATABASE_ID"])
-#     container = db.get_container_client("dou")
-
-#     def _upsert(pub):
-#         pub.update({"data": str(pub["data"])})
-
-#         try:
-#             container.upsert_item(pub)
-#         except:
-#             pub.update({"conteudo": pub["conteudo"][0:2500]})
-#             container.upsert_item(pub)
-
-#     total = 0
-#     with concurrent.futures.ThreadPoolExecutor() as executor:
-#         for _ in executor.map(_upsert, [i.to_dict() for i in df.iloc]):
-#             total += 1
-#             print(f"({total}/{len(df)}) = {round((total/len(df))*100, 2)}%")

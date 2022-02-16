4375eb9c5ba6d2916d2cb44549a31122af6e2d64
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-02-16 15:04:32 -0300

    Mover critérios R1 para o escopo

--- src/filtro/categorias/por_escopo.py
+++ src/filtro/categorias/por_escopo.py
@@ -2,6 +2,14 @@ from ..filtro import Filtro, Criterio
 
 
 class FiltragemPorEscopo(Filtro):
+    def especificos(self):
+        yield from [
+            Criterio(  # M1
+                self.tipo_normativo.igual("Portaria") & self.secao.contem("DO1"),
+                motivo="Portaria do COAF na seção 1",
+            )
+        ]
+
     def menciona_o_banco_central(self):
         yield from [
             Criterio(
@@ -9,10 +17,26 @@ class FiltragemPorEscopo(Filtro):
                 & self.conteudo.contem(r"Banco Central"),
                 motivo="Gabinete de Segurança Institucional no escopo e menciona Banco Central no conteúdo",
             ),
-            Criterio( #R2A2
+            Criterio(  # R2A2
                 self.escopo.contem(r"Secretaria Especial do Tesouro e Orçamento")
                 & self.conteudo.contem(r"Banco Central")  # T2022-2-13-Carlos
                 & self.titulo.notna(),
                 motivo="'Secretaria Especial do Tesouro e Orçamento' no escopo e menciona o Banco Central no conteúdo",
+            ),
+            Criterio(  # R1
+                self.titulo.contem(r"Superintendência de Seguros Privados")
+                & self.ementa.contem("Banco Central"),
+                motivo="Publicação da SUSEP que menciona o Banco Central na Ementa",
+            ),
+            Criterio(  # R1
+                self.titulo.contem(
+                    r"Superintendência Nacional de Previdência Complementar"
+                )
+                & self.ementa.contem("Banco Central"),
+                motivo="Publicação da PREVIC que menciona o Banco Central na Ementa",
+            ),
+            Criterio(  # R1
+                self.titulo.contem(r"COAF") & self.ementa.contem("Banco Central"),
+                motivo="Publicação do COAF que menciona o Banco Central na Ementa",
             ),
         ]
--- src/filtro/categorias/por_titulo.py
+++ src/filtro/categorias/por_titulo.py
@@ -16,21 +16,9 @@ class FiltragemPorTitulo(Filtro):
 
     def menciona_o_banco_central(self):
         yield from [
-            Criterio(  # R1
-                self.titulo.contem(r"SUSEP") & self.ementa.contem("Banco Central"),
-                motivo="Publicação da SUSEP que menciona o Banco Central na Ementa",
-            ),
-            Criterio(  # R1
-                self.titulo.contem(r"PREVIC") & self.ementa.contem("Banco Central"),
-                motivo="Publicação da PREVIC que menciona o Banco Central na Ementa",
-            ),
-            Criterio(  # R1
-                self.titulo.contem(r"COAF") & self.ementa.contem("Banco Central"),
-                motivo="Publicação do COAF que menciona o Banco Central na Ementa",
-            ),
             Criterio(  # Conversa com Carlos e Ligiane no dia 04/01/2022
                 self.titulo.contem(r"PORTARIA SETO")
                 & self.conteudo.contem("Banco Central"),
                 motivo="Portaria SETO que menciona o Banco Central no Conteúdo",
-            ),
+            )
         ]

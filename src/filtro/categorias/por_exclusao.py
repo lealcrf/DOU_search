3c1c185522df6d0ecba7d6a95a9bff10eaab85fe
Matheus Santiago <matheus.nilo.santiago@gmail.com>
2022-02-21 09:33:00 -0300

    .

--- src/filtro/categorias/por_exclusao.py
+++ src/filtro/categorias/por_exclusao.py
@@ -2,10 +2,11 @@ from ..filtro import Criterio, Filtro
 
 
 class FiltragemPorExclusao(Filtro):
-    def geral(self):
-        return self.query(
-            ~(
-                (self.df.tipo_normativo == "Instrução Normativa")
-                & self.contains(self.df.escopo, "Banco Central")
-            )
-        )
+    pass
+    # def geral(self):
+    #     return self.query(
+    #         ~(
+    #             (self.df.tipo_normativo == "Instrução Normativa")
+    #             & self.contains(self.df.escopo, "Banco Central")
+    #         )
+    #     )
